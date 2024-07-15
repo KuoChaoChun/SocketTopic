c20d1ddbb87ba53635899d7e422c5f58559b0745
KathyKuo <ttchaochun@gmail.com>
2024-07-16 02:23:45 +0800

    新增Client、Server Form

---
+++ ClientForm/ClientForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SocketTopic.Interface;
+using SocketTopic.Factory;
+using SocketTopic.Services;
+
+namespace ClientForm
+{
+    public partial class ClientForm : Form
+    {
+        private IClient _client;
+        private Socket clientSocket;
+
+        public ClientForm()
+        {
+            InitializeComponent();
+
+            _client = SocketFactory.CreateClient();
+        }
+
+        private void RequestBtn_Click(object sender, EventArgs e)
+        {
+            string serverIP = IpTxt.Text.Trim();
+            int serverPort = 0;
+
+            if (!int.TryParse(PortTxt.Text.Trim(), out serverPort))
+            {
+                MessageBox.Show("請輸入有效的Port。");
+                return;
+            }
+
+            Task.Run(() => SendRequest(serverIP, serverPort, FileNameTxt.Text.Trim()));
+
+            #region 原始OK            
+            //await Task.Run(() => StartClient(serverIP, serverPort));
+            #endregion
+        }
+
+        private async Task SendRequest(string serverIP, int serverPort, string fileName)
+        {
+            try
+            {
+                _client.Connect(serverIP, serverPort);
+                AppendLog($"已连接到服务器 {serverIP}:{serverPort}");
+
+                _client.Send(fileName);
+
+                await ReceiveResponse(_client);
+
+                _client.Disconnect();
+                AppendLog("已断开连接");
+            }
+            catch (Exception ex)
+            {
+                AppendLog("错误: " + ex.Message);
+            }
+        }
+
+        private async Task ReceiveResponse(IClient client)
+        {
+            var buffer = new byte[1024];
+            var receivedBytes = await Task<int>.Factory.FromAsync(
+                client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, null, client),
+                client.EndReceive);
+            string receivedData = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+            AppendLog("从服务器接收到的数据：" + receivedData);
+        }
+
+        private void AppendLog(string message)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => ResultTxt.AppendText(message + Environment.NewLine)));
+            }
+            else
+            {
+                ResultTxt.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        #region 原始測試OK
+        //private async Task StartClient(string serverIP, int serverPort)
+        //{
+        //    try
+        //    {
+        //        clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        //        IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 8081);
+        //        clientSocket.Connect(serverEndPoint);
+
+        //        AppendLog("已连接到服务器。");
+                                
+        //        string dataToSend = "Hello, Server!";
+        //        byte[] data = Encoding.UTF8.GetBytes(dataToSend);
+        //        clientSocket.Send(data);
+
+        //        await ReceiveDataAsync(clientSocket);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        AppendLog("客户端错误: " + ex.Message);
+        //    }
+        //}
+
+        //private async Task ReceiveDataAsync(Socket client)
+        //{
+        //    byte[] buffer = new byte[1024];
+
+        //    while (true)
+        //    {
+        //        int receivedBytes = await Task<int>.Factory.FromAsync(
+        //            client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, null, client),
+        //            client.EndReceive);
+
+        //        if (receivedBytes == 0) break; // 连接已关闭
+
+        //        string receivedData = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+        //        AppendLog("从服务器接收到的数据：" + receivedData);
+        //    }
+        //}
+
+        //private void AppendLog(string message)
+        //{
+        //    ResultTxt.Invoke(new Action(() => ResultTxt.AppendText(message + Environment.NewLine)));
+        //}
+        #endregion
+    }
+}
---
+++ ServerForm/ServerForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SocketTopic.Factory;
+using SocketTopic.Interface;
+
+namespace ServerForm
+{
+    public partial class ServerForm : Form
+    {
+        private Socket serverSocket;
+
+        public ServerForm()
+        {
+            InitializeComponent();
+
+            //Task.Run(() => StartServer());
+
+            IServer server = SocketFactory.CreateServer();
+            Task.Run(() => server.Start()); 
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, 8080);
+                serverSocket.Bind(endPoint);
+                serverSocket.Listen(10);
+
+                AppendLog("服务器已启动，等待客户端连接...");
+
+                while (true)
+                {
+                    Socket client = serverSocket.Accept();
+                    AppendLog("客户端已连接。");
+
+                    UpdateClientInfo(endPoint.Address.ToString(), endPoint.Port);
+
+                    Thread clientThread = new Thread(() => HandleClient(client));
+                    clientThread.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendLog("服务器错误: " + ex.Message);
+            }
+        }
+
+        private void HandleClient(Socket client)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int receivedBytes = client.Receive(buffer);
+                string receivedData = Encoding.UTF8.GetString(buffer, 0, receivedBytes);
+                AppendLog("接收到的数据：" + receivedData);
+
+                string response = "数据已接收";
+                byte[] responseData = Encoding.UTF8.GetBytes(response);
+                client.Send(responseData);
+
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                AppendLog("处理客户端错误: " + ex.Message);
+            }
+        }
+
+        private void AppendLog(string message)
+        {
+            //ResultTxt.Invoke(new Action(() => ResultTxt.AppendText(message + Environment.NewLine)));
+
+            if (InvokeRequired)
+            {
+                Invoke(new Action(() => ResultTxt.AppendText(message + Environment.NewLine)));
+            }
+            else
+            {
+                ResultTxt.AppendText(message + Environment.NewLine);
+            }
+        }
+
+        private void UpdateClientInfo(string clientIP, int clientPort)
+        {
+            IpTxt.Invoke(new Action(() => IpTxt.Text = clientIP));
+            PortTxt.Invoke(new Action(() => PortTxt.Text = clientPort.ToString()));
+        }
+    }
+}
